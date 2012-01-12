b02a64ebf0f7a91b0d023259d13302914e7fec35
Kelly Trageser <kelty@cablespeed.com>
2012-01-12 13:51:38 -0500

    Created a Users resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4146b44afb7194dc034a23e94078366fc33fae2ee9fe2a7b03c6239aea994058f82d001c1cb857967f228fd262673d2f9e81cc6dc871670f640fc44b653c85fe'
